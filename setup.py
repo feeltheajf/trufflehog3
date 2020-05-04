2a0b2df918d6e795e3b39e2f10a636aebcb1624c
Ilya Radostev <feeltheajf@gmail.com>
2020-05-04 13:02:14 +0300

    v2.0.0 release

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="truffleHog3",
-    version="2.0.0b3",
+    version="2.0.0",
     packages=["truffleHog3", "truffleHog3.lib"],
     license="GNU",
     description="Find secrets in your codebase.",
