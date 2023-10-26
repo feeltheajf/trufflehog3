953b56ce5e324dc540661abb0bd47e592eff282b
Ilya Radostev <feeltheajf@gmail.com>
2023-10-27 00:34:53 +0300

    [SECURITY] Bump GitPython version

--- trufflehog3/__init__.py
+++ trufflehog3/__init__.py
@@ -8,7 +8,7 @@ from pathlib import Path
 from trufflehog3 import helper
 
 __NAME__ = "trufflehog3"
-__VERSION__ = "3.0.7"
+__VERSION__ = "3.0.8"
 
 HERE = Path(__file__).parent
 STATIC_DIR = HERE / "static"
